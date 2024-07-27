de3338f0399f84b82c3236f2576443e97145d2c5
Abhishek kumar arya <abhishek.kr.arya004@gmail.com>
2024-07-27 19:06:00 +0530

    This is first GIT commit

---
+++ TESTNGDEMO/src/pack1/com/BASIC.java
@@ -0,0 +1,57 @@
+package pack1.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
+
+public class BASIC {
+	
+	
+	WebDriver driver = new ChromeDriver();
+	
+	
+	@Test
+	public void test1() {
+		
+		
+		
+
+		// WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+		
+		
+		//System.out.println("This is first TestNG1  code");
+		
+		
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+		
+		//System.out.println("This is first TestNG2 code");
+		
+		WebElement textBox = driver.findElement(By.name("my-text"));
+
+		WebElement submitButton = driver.findElement(By.cssSelector("button"));
+		
+		textBox.sendKeys("Selenium");
+		submitButton.click();
+		
+	}
+	
+	
+	@Test
+	public void test3() {
+		
+		
+		System.out.println("This is first TestNG3 code");
+		
+	}
+
+}
