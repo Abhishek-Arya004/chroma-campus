fa08c013d77acecfadef761855d8d28eeb93af4d
Abhishek kumar arya <abhishek.kr.arya004@gmail.com>
2024-09-11 23:18:35 +0530

    First push

---
+++ FIRST_JAVA_10_09_2024/src/com/demo/FIRSTJAVA.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+public class FIRSTJAVA {
+	
+	int a ;//instance variable
+	
+	static int c ;//static variable
+
+	
+	public static void main(String args[]) {
+		
+		int b ;// local varible
+	
+	
+	System.out.println("This is test code");
+		
+	
+	
+	}
+
+}
