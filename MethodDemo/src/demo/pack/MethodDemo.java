1b7274da4ebfa29ea64ffdec3c4870533eb6db0b
Abhishek kumar arya <abhishek.kr.arya004@gmail.com>
2024-09-12 01:23:48 +0530

    commit changes

---
+++ MethodDemo/src/demo/pack/MethodDemo.java
@@ -0,0 +1,35 @@
+package demo.pack;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+	Simple d = new Simple();
+		
+		//d.display();//method calling
+		
+		//d.display1();
+		
+		
+	//	d.addition(7, 8); // method calling
+	
+	int c= d.multiplication(3, 4);
+		
+		
+	System.out.println(c);
+
+	}
+	
+	
+	public void subtraction (int a , int b) {
+		   
+		
+		System.out.println(a-b); 
+		
+		
+
+	}
+
+} 
---
+++ MethodDemo/src/demo/pack/Simple.java
@@ -0,0 +1,48 @@
+package demo.pack;
+
+public class Simple {
+	
+	//method declaration
+	//default
+	
+	public void display() {  
+   
+		System.out.println("Hello"); 
+		
+		display1();
+
+	}
+	
+	
+	
+	public void display1() {
+		   
+		System.out.println("Hello Team"); 
+		
+		
+
+	}
+	
+	//
+	public void addition (int a , int b) {
+		   
+		
+		System.out.println(a+b); 
+		
+		
+
+	}
+	
+	
+public int multiplication (int a , int b) {
+		   
+					
+		return a*b;
+
+	}
+
+	
+	
+	
+
+}
